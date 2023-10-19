ec414d88ddab4c86b0ad3dcc215100dc7923bb83
LKyng <116057000+LKyng@users.noreply.github.com>
2023-10-20 00:28:18 +1300

    Finished things

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,78 @@
+let particles = [];
+let mainImg;
+let firstRun = true
 
-// vocal, drum, bass, and other are volumes ranging from 0 to 100
-function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Helvetica'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
+class Particle {
+  constructor(fillColor, size) {
+    this.pos = createVector(0,0,0);
+    this.vel = p5.Vector.random3D().mult(random(1, 3));
+    this.fillColor = fillColor;
+    this.size = size;
+  }
 
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
+  update() {
+    this.pos.add(this.vel);
+  }
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+  show() {
+    push();
+    noStroke();
+    fill(this.fillColor);
+    translate(this.pos.x, this.pos.y, this.pos.z);
+    sphere(2 * (this.size / 20));
+    pop();
+  }
 }
+
+function draw_one_frame(words, vocal, drum, bass, other, counter) {
+  if (firstRun){
+    rectMode(CENTER);
+    mainImg = loadImage('img/logo_0.PNG');
+    firstRun = false
+  }
+
+  background(0, 0, 30);
+
+  directionalLight([255], createVector(0, 1, -1));
+
+  let ParticleFill;
+
+  if (other > 75) {
+    for (var i = 0; i < 10; i++) {
+      ParticleFill = color(random(50, 90), random(0, 20), random(110, 150));
+      var p = new Particle(ParticleFill, vocal);
+      particles.push(p);
+    }
+  }
+  if (bass > 65) {
+    ParticleFill = color(random(0, 20), random(120, 160), random(0, 40));
+    for (var i = 0; i < 10; i++) {
+      var p = new Particle(ParticleFill, vocal);
+      particles.push(p);
+    }
+  }
+  if (drum > 65) {
+    ParticleFill = color(random(130, 170), random(0, 30), random(0, 20));
+    for (var i = 0; i < 10; i++) {
+      var p = new Particle(ParticleFill, vocal);
+      particles.push(p);
+    }
+  }
+
+  for (var i = particles.length - 1; i >= 0; i--) {
+    if (dist(particles[i].pos.x, particles[i].pos.y, particles[i].pos.z, 0, 0, 0) < 500) {
+      particles[i].update();
+      particles[i].show();
+    } else {
+      particles.splice(i, 1);
+    }
+  }
+
+  push();
+  let zPosition = 550;
+  let logoSize = map(vocal, 0, 100, 20, 70);
+  translate(0, 0, zPosition);
+  image(mainImg, -logoSize / 2, -logoSize / 2, logoSize, logoSize);
+  pop();
+}
+
